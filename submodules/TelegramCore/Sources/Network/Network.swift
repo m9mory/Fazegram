ec65ef714f09e264de97fec6e1319ea01cec72dc
m9mory <cowenlachelle@gmail.com>
2026-07-06 17:59:37 +0300

    Revert iCloud enable - still causes launch crash on sideload

--- submodules/TelegramCore/Sources/Network/Network.swift
+++ submodules/TelegramCore/Sources/Network/Network.swift
@@ -1303,10 +1303,7 @@ class Keychain: NSObject, MTKeychain {
 }
 #if os(iOS)
 func makeCloudDataContext(encryptionProvider: EncryptionProvider) -> CloudDataContext? {
-    if #available(iOS 10.0, *) {
-        return CloudDataContextImpl(encryptionProvider: encryptionProvider)
-    } else {
-        return nil
-    }
+    // Disabled for sideload compatibility - CloudKit requires entitlements
+    return nil
 }
 #endif
